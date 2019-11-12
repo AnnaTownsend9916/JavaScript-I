f87072f5ba4b5cb69332ed8375bb74a60f1b8e94
Anna <townsendanna15@gmail.com>
2019-11-11 23:13:09 -0500

    yassss challenge three was way easier this time, I created functions inside of my objects

--- assignments/objects.js
+++ assignments/objects.js
@@ -32,7 +32,10 @@ const Kennan = {
   "id": 2,
   "name": "Kennan",
   "email": "kdiben1@tinypic.com",
-  "gender": "M"
+  "gender": "M",
+  "speak":function(){
+    return "Hello my name is Kennan!"
+  }
 }
 
 const Keven = {
@@ -53,7 +56,10 @@ const Antonietta = {
   "id": 5,
   "name": "Antonietta",
   "email": "adaine5@samsung.com",
-  "gender": "F"
+  "gender": "F",
+  "multiplyNums":function(a,b){
+    return a*b
+  }
 }
 
 
@@ -74,10 +80,10 @@ console.log(Antonietta.gender)
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
-
+console.log(Kennan.speak)
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
-
+console.log(Antonietta.multiplyNums(3,4))
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
 // ==== Stretch Challenge: Nested Objects and the this keyword ==== 
